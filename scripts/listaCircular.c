294cb61ea6c636d6eb3856fcf307a3ad82b92ef2
Daniel Tiezzi <dtiezzi@usp.br>
2018-10-30 20:34:41 -0300

    Update listaCircular.c

--- scripts/listaCircular.c
+++ scripts/listaCircular.c
@@ -11,8 +11,9 @@ typedef struct Lista {
 void menu();
 void inserir(Lista **ini);
 void listar(Lista *ini);
-Lista* localizar(Lista *ini);
-//Lista* remover(Lista **ini, int *v);
+void consultar(Lista *ini);
+Lista* localizar(Lista *ini, int v);
+Lista* remover(Lista **ini);
 
 void main(void) {
 
@@ -29,14 +30,17 @@ void main(void) {
                 listar(inicio);
                 break;
             case '3':
-                localizar(inicio);
+                consultar(inicio);
                 break;
             case '4':
+                remover(&inicio);
+                break;
+            case '5':
                 exit(0);
                 break;
         }
 
-    } while (op != '4');
+    } while (op != '5');
 }
 
 
@@ -45,7 +49,8 @@ void menu() {
     printf("\n1 - Inserir novo registro.");
     printf("\n2 - Listar registros existentes.");
     printf("\n3 - Localiza registro.");
-    printf("\n4 - Sair.");
+    printf("\n4 - Remove registro.");
+    printf("\n5 - Sair.");
     printf("\nDigite a opção desejada:");
     printf("\n");
     
@@ -75,42 +80,90 @@ void inserir(Lista **ini) {
 
 void listar(Lista *ini) {
 
-    Lista *aux = ini;
-    do {
-        printf("\nRA: %d", aux->RA);
-        printf("\nNome: %s\n", aux->nome);
-        aux = aux->prox;
-    } while(aux != ini);
-}
-
-Lista* localizar(Lista *ini) {
-
-    int v;
-    printf("\nDigite o registro que deseja localizar: ");
-    scanf("%d", &v);
-    if (ini->RA == v) {
-        printf("\nRA: %d localizado!\n", v);
-        return ini;
-    } else {
+    if (ini == NULL)
+        printf("\nNao há registros no arquivo!\n");
+    else {
         Lista *aux = ini;
-        int n;
-        while (aux->prox->RA != v) {
+        do {
+            printf("\nRA: %d", aux->RA);
+            printf("\nNome: %s\n", aux->nome);
             aux = aux->prox;
-            if (aux->prox == ini) {
-                printf("\nRegistro não encontrado!\n");
-                return NULL;
-            } 
-        }
-        printf("\nRA: %d localizado!\n", v);
-        return aux->prox;
+        } while(aux != ini);
     }
 }
 
-/*
-Lista* remover(Lista *ini, int *v) {
+void consultar(Lista *ini) {
 
-    printf("\nDigite o registro que deseja remover: ");
-    scanf("%d", v);
-    localiza(ini, *v);
+    if (ini == NULL)
+        printf("\nNao há registros no arquivo!\n");
+    else {
+        int v;
+        printf("\nDigite o registro que deseja localizar: ");
+        scanf("%d", &v);
+        if (ini->RA == v) {
+            printf("\nRA: %d localizado!\n", v);
+        } else {
+            Lista *aux = ini;
+            int n;
+            while (aux->prox->RA != v) {
+                aux = aux->prox;
+                if (aux->prox == ini) {
+                    printf("\nRegistro %d não encontrado!\n", v);
+                } 
+            }
+            printf("\nRA: %d localizado!\n", v);
+        }
+    }
+}
 
-}*/
+Lista* localizar(Lista *ini, int v) {
+
+        Lista *aux = ini;
+        int n;
+        do {
+            aux = aux->prox; 
+            if (aux->prox->RA == v)
+                return aux;
+        } while (aux != ini);
+        return NULL;
+}
+
+Lista* remover(Lista **ini) {
+
+    if (*ini == NULL)
+        printf("\nNao há registros no arquivo!\n");
+    else {
+        if ((*ini)->prox == *ini) {
+            char op;
+            printf("\nO único registro no banco é o RA: %d.\n", (*ini)->RA);
+            printf("Tem ceteza que deseja remove-lo?\nDigite 1 para SIM ou 0 para nao.");
+            scanf(" %c", &op);
+            if (op == '0') {
+                exit(0);
+            } else {
+                Lista *aux = *ini;
+                free(aux);
+                *ini = NULL;
+            }
+        } else {
+            int rg;
+            printf("\nDigite o registro que deseja remover: ");
+            scanf("%d", &rg);
+            Lista *pre = localizar(*ini, rg);
+            Lista *aux = pre->prox;
+            if (aux == NULL)
+                printf("\nRegistro nao encontrado no banco de dados!\n");
+            else {
+                if (aux == *ini) {
+                    pre->prox = aux->prox;
+                    *ini = pre;
+                    free(aux);
+                } else {
+                    pre->prox = aux->prox;
+                    free(aux);
+                    //printf("\nDeseja remover o RA %d?\n", aux->prox->RA);
+                }
+            }
+        }
+    }
+}
